f2eed2eedb613752dd9e896868001fd83ee53266
Sammie Collinson <heroldsamantha@gmail.com>
2022-01-22 21:34:20 -0500

    moved API calls to the App file, added styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import './styles/App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Books from './components/Books';
 import books from './books';
 
 
 
 const App = () => {
+  
+  const [bookState, setBookState] = useState([])
+  const API = `https://openlibrary.org`
+  const ISBNList = books.map((book) => (
+    `${API}/isbn/${book.ISBN}.json`
+    ))
+    
+    useEffect(() => {
+      async function getISBNInfo() {
+        let ISBNInfo =[];
+        for(let i=0; i<ISBNList.length; i++) {
+          const res = await axios.get(`${ISBNList[i]}`)
+          ISBNInfo.push(res.data)
+        }
+        const works = ISBNInfo.map((work) => {
+          let json = work.works[0].key
+          return `${API}${json}.json`
+        })
+        async function getWorks() {
+              let workArr = [];
+              for(let i=0; i<works.length; i++) {
+              const response = await axios.get(`${works[i]}`)
+              workArr.push(response.data)
+              let coverID = workArr[i].covers[0]
+              workArr[i].coverArt = `https://covers.openlibrary.org/b/id/${coverID}-L.jpg`
+              setBookState(workArr)
+      
+            }
+          }
+        getWorks()
+    } getISBNInfo()
+  },[books])
+console.log(bookState)
+
   return (
     <div className="App">
       <h2>worseReads</h2>
-      <Books books={books} />
+      <Books bookState={bookState} />
     </div>
   );
 }
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,50 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 
 const Books = (props) => {
-    const [bookState, setBookState] = useState('')
-    const API = 'https://openlibrary.org'
-
-    const isbnList = props.books.map((book) => (
-        `${API}/isbn/${book.ISBN}.json`
-    ))
-
-
-    useEffect(() => {
-        async function getBooks () {
-                let apiCall =[];
-                for(let i=0; i<isbnList.length; i++) {
-                const res = await axios.get(`${isbnList[i]}`)
-                apiCall.push(res.data)
-            }
-            const works = apiCall.map((work) => {
-                let json = work.works[0].key
-               return `${API}${json}.json`
-            })
-            async function getWorks() {
-                let workArr = [];
-                for(let i=0; i<works.length; i++) {
-                    const response = await axios.get(`${works[i]}`)
-                    workArr.push(response.data)
-                    let coverID = workArr[i].covers[0]
-                    workArr[i].coverArt = `https://covers.openlibrary.org/b/id/${coverID}-L.jpg`
-                    setBookState(workArr)
-
-                }
-            }
-            getWorks()
-            
-        } getBooks()
-    },[isbnList])
-
-    console.log(bookState)
-    
     return(
-        <div>
+        <div className="grid">
             {
-                bookState.map((book) => (
-                    <h3 key={book.key}>{book.title}</h3>
+                props.bookState.map((book) => (
+                    <div key={book.key} className="card">
+                     <h3>{book.title}</h3>
+                     <button>View Details</button>
+                     <img src={book.coverArt} alt="book cover"></img>
+                    </div>
                 ))
             }
         </div>
